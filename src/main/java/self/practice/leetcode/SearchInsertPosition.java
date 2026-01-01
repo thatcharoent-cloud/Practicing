bb0b581bde65122be06069942e65845e9566df4f
tttoppp <thatchaphong.tha@gmail.com>
2026-01-01 14:39:48 -0500

    search insert position

---
+++ src/main/java/self/practice/leetcode/SearchInsertPosition.java
@@ -0,0 +1,33 @@
+package self.practice.leetcode;
+
+public class SearchInsertPosition {
+	
+	public int searchInsert(int[] nums, int target) {
+		
+		int pos = 0;
+		
+		for(int n : nums) {
+			
+			if(n >= target) {
+				return pos;
+			}
+			
+			pos++;
+			
+		}
+		
+		return pos;
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		SearchInsertPosition s = new SearchInsertPosition();
+		
+		System.out.println(s.searchInsert(new int[]{1, 3, 5, 6}, 5));
+		System.out.println(s.searchInsert(new int[]{1, 3, 5, 6}, 2));
+		System.out.println(s.searchInsert(new int[]{1, 3, 5, 6}, 7));
+		
+	}
+	
+}
