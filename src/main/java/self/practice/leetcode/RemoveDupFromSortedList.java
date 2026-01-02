3a2b7f16e6a3aff871b37eb4081e7e33b63233c2
tttoppp <thatchaphong.tha@gmail.com>
2026-01-02 00:15:21 -0500

    remove duplicate from linked list

---
+++ src/main/java/self/practice/leetcode/RemoveDupFromSortedList.java
@@ -0,0 +1,64 @@
+package self.practice.leetcode;
+
+public class RemoveDupFromSortedList {
+	
+	public ListNode deleteDuplicates(ListNode head) {
+		
+		ListNode result, tmpNode;
+		result = new ListNode(Integer.MIN_VALUE);
+		tmpNode = result;
+		
+		int tmp = 0;
+		
+		while((head!=null)) {
+			
+			tmp = head.val;
+			
+			if(tmp != tmpNode.val) {
+				
+				tmpNode.next = new ListNode(tmp);
+				tmpNode = tmpNode.next;
+				
+			}
+			
+			head = head.next;
+			
+		}
+		
+		result = result.next;
+		
+		return result;
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		RemoveDupFromSortedList r = new RemoveDupFromSortedList();
+		
+		ListNode n;
+		
+		n = new ListNode(1);
+		n.next = new ListNode(1);
+		n.next.next = new ListNode(2);
+		r.deleteDuplicates(n);
+		
+		n = new ListNode(1);
+		n.next = new ListNode(1);
+		n.next.next = new ListNode(2);
+		n.next.next.next = new ListNode(3);
+		n.next.next.next.next = new ListNode(3);
+		r.deleteDuplicates(n);
+		
+	}
+	
+	public static class ListNode{
+	
+	int val;
+	ListNode next;
+	ListNode(){}
+	ListNode(int val) { this.val = val; }
+	ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+	
+}
+	
+}
