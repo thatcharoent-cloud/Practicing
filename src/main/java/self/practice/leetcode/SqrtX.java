9c83d249da6be76bfa3c065554decd28bf3f26da
tttoppp <thatchaphong.tha@gmail.com>
2026-01-01 19:19:20 -0500

    sqrt x

---
+++ src/main/java/self/practice/leetcode/SqrtX.java
@@ -0,0 +1,25 @@
+package self.practice.leetcode;
+
+public class SqrtX {
+	
+	public int mySqrt2(int x) {
+		return (int)Math.sqrt(x);
+	}
+	
+	public int mySqrt(int x) {
+		return (int)Math.pow(8, 0.5);
+	}
+	
+	public static void main(String[] args){
+		
+		SqrtX s = new SqrtX();
+		
+		System.out.println(s.mySqrt(4));
+		System.out.println(s.mySqrt(8));
+		
+		System.out.println(s.mySqrt2(4));
+		System.out.println(s.mySqrt2(8));
+		
+	}
+	
+}
