e5b6c6b40747ae75aa83b41d9cd7f92366932b39
tttoppp <thatchaphong.tha@gmail.com>
2026-01-08 17:14:06 -0500

    update

--- src/main/java/self/practice/leetcode/BinaryTreeInorderTraversal.java
+++ src/main/java/self/practice/leetcode/BinaryTreeInorderTraversal.java
@@ -72,22 +72,22 @@ public class BinaryTreeInorderTraversal {
 		
 	}
 	
-}
-
-class TreeNode{
+	public static class TreeNode{
+		
+		int val;
+		
+		TreeNode left;
+		TreeNode right;
+		
+		TreeNode(){}
+		TreeNode(int val) { this.val = val; }
+		
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
 	
-	int val;
-	
-	TreeNode left;
-	TreeNode right;
-	
-	TreeNode(){}
-	TreeNode(int val) { this.val = val; }
-	
-	TreeNode(int val, TreeNode left, TreeNode right) {
-		this.val = val;
-		this.left = left;
-		this.right = right;
 	}
 	
 }
