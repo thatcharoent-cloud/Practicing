30cffd0bbb8460eb905fbceea70e6ce2a59a3566
tttoppp <thatchaphong.tha@gmail.com>
2026-01-08 17:47:43 -0500

    check is that the same tree or not

---
+++ src/main/java/self/practice/leetcode/SameTree.java
@@ -0,0 +1,81 @@
+package self.practice.leetcode;
+
+public class SameTree {
+	
+	public boolean isSameTree(TreeNode p, TreeNode q) {
+		
+		return checkTree(p, q);
+		
+	}
+	
+	private boolean checkTree(TreeNode p, TreeNode q) {
+		
+		if(p==null && q==null) { return true; }
+		
+		if(p==null && q!=null) { return false; }
+		if(p!=null && q==null) { return false; }
+		
+		if(p.val != q.val) { return false;}
+		
+		if(!checkTree(p.left, q.left)) { return false; }
+			
+		if(!checkTree(p.right, q.right)) { return false; }
+		
+		return true;
+		
+	}
+
+	public static void main(String[] args) {
+		
+		SameTree b = new SameTree();
+		
+		TreeNode p, q;
+		
+		p = new TreeNode(1);
+		p.left = new TreeNode(2);
+		p.right = new TreeNode(3);
+		
+		q = new TreeNode(1);
+		q.left = new TreeNode(2);
+		q.right = new TreeNode(3);
+		
+		System.out.println(b.isSameTree(p, q));
+		
+		p = new TreeNode(1);
+		p.left = new TreeNode(2);
+		
+		q = new TreeNode(1);
+		q.right = new TreeNode(2);
+		
+		System.out.println(b.isSameTree(p, q));
+		
+		p = new TreeNode(1);
+		p.left = new TreeNode(2);
+		p.right = new TreeNode(1);
+		
+		q = new TreeNode(1);
+		q.left = new TreeNode(1);
+		q.right = new TreeNode(2);
+		
+		System.out.println(b.isSameTree(p, q));
+	}
+	
+	public static class TreeNode{
+	
+		int val;
+		
+		TreeNode left;
+		TreeNode right;
+		
+		TreeNode(){}
+		TreeNode(int val) { this.val = val; }
+		
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	
+	}
+	
+}
