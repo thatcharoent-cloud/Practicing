ced5b411bf75246eed94975a6aeb21a1312535c7
tttoppp <thatchaphong.tha@gmail.com>
2026-01-09 01:01:02 -0500

    check binary tree is symmetric or not

---
+++ src/main/java/self/practice/leetcode/SymmetricTree.java
@@ -0,0 +1,87 @@
+package self.practice.leetcode;
+
+public class SymmetricTree {
+	
+	public boolean isSymmetric(TreeNode root) {
+		
+		if(root == null) {
+			return true;
+		}
+		
+		return check(root.left, root.right);
+		
+	}
+
+	private boolean check(TreeNode p, TreeNode q) {
+		
+		if(p==null && q==null) {
+			return true;
+		}
+		
+		if(p==null || q==null) {
+			return false;
+		}
+		
+		if(p.val != q.val) {
+			return	false;
+		}
+		
+		return check(p.left, q.right)&&check(p.right, q.left);
+		
+	}
+
+	public static void main(String[] args) {
+		
+		SymmetricTree b = new SymmetricTree();
+		
+		TreeNode root;
+		
+		root = new TreeNode(1);
+		
+		root.left = new TreeNode(2);
+		root.left.left = new TreeNode(3);
+		root.left.right = new TreeNode(4);
+		
+		root.right = new TreeNode(2);
+		root.right.left = new TreeNode(4);
+		root.right.right = new TreeNode(3);
+		
+		System.out.println(b.isSymmetric(root));
+		System.out.println();
+		
+		root = new TreeNode(1);
+		
+		root.left = new TreeNode(2);
+		root.left.right = new TreeNode(3);
+		
+		root.right = new TreeNode(2);
+		root.right.right = new TreeNode(3);
+		
+		System.out.println(b.isSymmetric(root));
+		System.out.println();
+		
+		root = new TreeNode(1);
+		root.left = new TreeNode(2);
+		root.right = new TreeNode(3);
+		System.out.println(b.isSymmetric(root));
+	}
+	
+	public static class TreeNode{
+	
+		int val;
+		
+		TreeNode left;
+		TreeNode right;
+		
+		TreeNode(){}
+		TreeNode(int val) { this.val = val; }
+		
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	
+	}
+	
+}
