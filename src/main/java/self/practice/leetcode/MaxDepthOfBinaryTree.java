be93abfee6d27cc56cd1e2512fd27c6f12bf0cf2
tttoppp <thatchaphong.tha@gmail.com>
2026-01-15 00:51:08 -0500

    max depth of binary tree

---
+++ src/main/java/self/practice/leetcode/MaxDepthOfBinaryTree.java
@@ -0,0 +1,58 @@
+package self.practice.leetcode;
+
+public class MaxDepthOfBinaryTree {
+	
+	public int maxDepth(TreeNode root) {
+		
+		if(root == null) { return 0; }
+		
+		return Math.max(
+				maxDepth(root.left), 
+				maxDepth(root.right)
+			) + 1;
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		MaxDepthOfBinaryTree m = new MaxDepthOfBinaryTree();
+		
+		TreeNode t;
+		
+		t = new TreeNode(3);
+		
+		t.left = new TreeNode(9);
+		
+		t.right = new TreeNode(20);
+		t.right.left = new TreeNode(15);
+		t.right.right = new TreeNode(7);
+		
+		System.out.println(m.maxDepth(t));
+		
+		t = new TreeNode(1);
+		
+		t.right = new TreeNode(2);
+		
+		System.out.println(m.maxDepth(t));
+		
+	}
+	
+	public static class TreeNode{
+	
+		int val;
+		
+		TreeNode left;
+		TreeNode right;
+		
+		TreeNode(){}
+		TreeNode(int val) { this.val = val; }
+		
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	
+	}
+	
+}
